011a0d24794ba84557832ba168e035c9ddabe4e6
kkssbbb <03337@naver.com>
2022-11-03 02:27:48 +0900

    feat: 홈페이지 레이아웃 구성

---
+++ src/views/home/js/bookDetail.js
@@ -0,0 +1,7 @@
+import $ from './utils.js/dom.js';
+
+$('.book-container').addEventListener('click', e => {
+  if (e.target.classList.contains('product')) console.log(e.target.src);
+});
+
+console.log('connect');
---
+++ src/views/home/js/index.js
@@ -0,0 +1,17 @@
+/* 
+
+- nav바 링크 클릭 시 해당페이지로 이동
+- 책 카테고리 클릭 시 받아온 api로 해당 카테고리 책 랜더링  
+- 책 이미지 클릭 시 상세정보 페이지로 이동
+
+*/
+
+import $ from './utils.js/dom.js';
+
+$('.book-container').addEventListener('click', e => {
+  if (e.target.classList.contains('product')) console.log(e.target.src);
+});
+
+console.log('connect');
+
+$('');
