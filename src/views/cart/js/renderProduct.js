15f7f0aea095369784f75b3b5962c59a29c78669
kkssbbb <03337@naver.com>
2022-11-06 23:07:25 +0900

    refactor: renderProduct 함수 리펙토링

--- src/views/cart/js/renderProduct.js
+++ src/views/cart/js/renderProduct.js
@@ -2,12 +2,21 @@ import $ from "../../utils/dom.js";
 // import store from "./js/store.js";
 
 const renderProduct = (producttList) => {
-  return producttList.map((book) => {
+  return producttList.forEach((book) => {
     $(".product-list").insertAdjacentHTML(
       "beforeend",
-      `<li> <input id="prodcut" type="checkbox" /> ${book}</li>`
+      `
+      <li id="product-${book}"> 
+        <label class="checkbox" id="${book}">
+          <input id="checkbox-${book}" type="checkbox" /> 
+        <label>
+        ${book}
+        <input id = "quantity-${book}" type = "number" value = "1" min = "1"/>
+        <span> ${book}} </span>
+      </li>
+      `
     );
   });
 };
-
+// input에 id 값으로 book 을 넣어 해당 상품 개수 확인
 export default renderProduct;
