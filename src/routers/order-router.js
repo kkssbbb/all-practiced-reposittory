137e4b940bfe564427936f8d973a1d8f9083e42c
kkssbbb <03337@naver.com>
2022-11-08 00:14:48 +0900

    fix: 로그인한 회원만 주문할 수 있도록 함

--- src/routers/order-router.js
+++ src/routers/order-router.js
@@ -1,12 +1,13 @@
 import { Router } from "express";
 import is from "@sindresorhus/is";
 
+import { loginRequired } from "../middlewares";
 import { orderService } from "../services";
 
 const orderRouter = Router();
 
 //주문 생성
-orderRouter.post("/orders", async (req, res, next) => {
+orderRouter.post("/orders", loginRequired, async (req, res, next) => {
   try {
     // Content-Type: application/json 설정을 안 한 경우, 에러를 만들도록 함.
     // application/json 설정을 프론트에서 안 하면, body가 비어 있게 됨.
