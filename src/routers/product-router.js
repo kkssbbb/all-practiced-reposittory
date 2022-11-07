07ee31c80d4c11edb8c9ef5a979e1188e55d1d53
kkssbbb <03337@naver.com>
2022-11-07 23:05:08 +0900

    feat:미들웨어 수정

--- src/routers/product-router.js
+++ src/routers/product-router.js
@@ -2,6 +2,7 @@ import { Router } from "express";
 import { productService } from "../services";
 const productRouter = Router();
 import { upload } from "../middlewares";
+import { productRequired } from "../middlewares";
 
 // productRouter.post("/profile", upload.single("image"), async (req, res) => {
 //   console.log(req.file);
@@ -9,93 +10,115 @@ import { upload } from "../middlewares";
 // });
 
 //상품 등록
-productRouter.post("/products", upload.single("imgUrl"), async (req, res) => {
-  const {
-    title,
-    price,
-    category,
-    author,
-    publisher,
-    publicationDate,
-    pageNumber,
-    summary,
-  } = req.body;
+productRouter.post(
+  "/products",
+  upload.single("imgUrl"),
+  productRequired,
+  async (req, res, next) => {
+    try {
+      const {
+        title,
+        price,
+        category,
+        author,
+        publisher,
+        publicationDate,
+        pageNumber,
+        summary,
+      } = req.body;
 
-  const imgUrl = req.file.path;
-  const content = await productService.addProduct({
-    imgUrl,
-    title,
-    price,
-    category,
-    author,
-    publisher,
-    publicationDate,
-    pageNumber,
-    summary,
-  });
-  res.json(content);
-});
+      const imgUrl = req.file.path;
+      const content = await productService.addProduct({
+        imgUrl,
+        title,
+        price,
+        category,
+        author,
+        publisher,
+        publicationDate,
+        pageNumber,
+        summary,
+      });
+      res.status(201).json(content);
+    } catch (error) {
+      next(error);
+    }
+  }
+);
 
 //상품 조회
-productRouter.get("/products/:id", async (req, res) => {
-  const productId = req.params.id;
-  const product = await productService.getProduct(productId);
-  res.status(201).json(product);
+productRouter.get("/products/:id", async (req, res, next) => {
+  try {
+    const productId = req.params.id;
+    const product = await productService.getProduct(productId);
+    res.status(200).json(product);
+  } catch (error) {
+    next(error);
+  }
 });
-//상품 전체 조회
-productRouter.get("/products", async (req, res) => {
-  const products = await productService.getProducts();
-  res.status(201).json(products);
-});
-//상품 카테고리별 조회
-productRouter.get("/products/category/:id", async (req, res) => {
-  const productsCategory = req.params.id;
-  const productsCategoryInfo = await productService.getProductsCategory(
-    productsCategory
-  );
 
-  res.status(201).json(productsCategoryInfo);
+//상품 전체 조회 및 카테고리 별 조회
+productRouter.get("/products", async (req, res, next) => {
+  try {
+    const { category } = req.query;
+    if (category !== undefined) {
+      const product = await productService.getProductsByCategory(category);
+      res.json({ error: null, data: product });
+    }
+    const products = await productService.getProducts();
+    res.status(201).json(products);
+  } catch (error) {
+    next(error);
+  }
 });
 
 //상품 업데이트
-productRouter.patch("/products/:id", async (req, res) => {
-  const productId = req.params.id;
+productRouter.patch("/products/:id", async (req, res, next) => {
+  try {
+    const productId = req.params.id;
 
-  const {
-    title,
-    price,
-    category,
-    author,
-    publisher,
-    publicationDate,
-    pageNumber,
-    summary,
-  } = req.body;
+    const {
+      title,
+      price,
+      category,
+      author,
+      publisher,
+      publicationDate,
+      pageNumber,
+      summary,
+    } = req.body;
 
-  const toUpdate = {
-    ...(title && { title }),
-    ...(price && { price }),
-    ...(category && { category }),
-    ...(author && { author }),
-    ...(publisher && { publisher }),
-    ...(publicationDate && { publicationDate }),
-    ...(pageNumber && { pageNumber }),
-    ...(summary && { summary }),
-  };
+    const toUpdate = {
+      ...(title && { title }),
+      ...(price && { price }),
+      ...(category && { category }),
+      ...(author && { author }),
+      ...(publisher && { publisher }),
+      ...(publicationDate && { publicationDate }),
+      ...(pageNumber && { pageNumber }),
+      ...(summary && { summary }),
+    };
 
-  const updatedProductInfo = await productService.updateProduct(
-    productId,
-    toUpdate
-  );
-  res.status(200).json(updatedProductInfo);
+    const updatedProductInfo = await productService.updateProduct(
+      productId,
+      toUpdate
+    );
+    res.json(updatedProductInfo);
+  } catch (error) {
+    next(error);
+  }
 });
 
 //상품 삭제
-productRouter.delete("/products/:id", async (req, res) => {
-  const productId = req.params.id;
-  const productsDelete = await productService.deleteProduct(productId);
+productRouter.delete("/products/:id", async (req, res, next) => {
+  try {
+    const productId = req.params.id;
+    const productsDelete = await productService.deleteProduct(productId);
 
-  res.status(201).json(productsDelete);
+    res.json(productsDelete);
+  } catch (error) {
+    next(error);
+  }
 });
 
 export { productRouter };
