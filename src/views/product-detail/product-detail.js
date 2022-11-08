e9eb060adf195054b6389a128b4d95617d02b970
kkssbbb <03337@naver.com>
2022-11-08 17:42:36 +0900

    feat: 시작페이지 수정

--- src/views/product-detail/product-detail.js
+++ src/views/product-detail/product-detail.js
@@ -1,59 +1,44 @@
-// import * as Api from "../api.js";
-// import $ from "../utils/dom.js";
-// import { getUrlParams, addCommas } from "../useful-functions.js";
+import * as Api from "../api.js";
+import $ from "../utils/dom.js";
+import { getUrlParams, addCommas } from "../useful-functions.js";
 
-const getUrlParams = () => {
-  const queryString = window.location.search;
-  const urlParams = new URLSearchParams(queryString);
+const webTitle = $("title");
+const addCartBtn = $(".add-cart-btn");
+const buyNowBtn = $(".buy-now-btn");
 
-  const result = {};
-
-  for (const [key, value] of urlParams) {
-    result[key] = value;
-  }
-
-  return result; //{ category : novel }
-};
-
-const addCommas = (n) => {
-  return n.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
-};
-
-const title = document.querySelector("title");
-const addCartBtn = document.querySelector(".add-cart-btn");
-const buyNowBtn = document.querySelector(".buy-now-btn");
-
-const bookCategory = document.querySelector(".book-category");
-const bookTitle = document.querySelector(".book-title");
-const bookAuthor = document.querySelector(".book-author");
-const bookPublisher = document.querySelector(".book-publisher");
-const bookPublicationDate = document.querySelector(".book-publicationDate");
-const bookPage = document.querySelector(".book-page");
-const bookPrice = document.querySelector(".book-price");
-const bookSummary = document.querySelector(".book-summary");
+const bookCategory = $(".book-category");
+const bookTitle = $(".book-title");
+const bookAuthor = $(".book-author");
+const bookPublisher = $(".book-publisher");
+const bookPublicationDate = $(".book-publicationDate");
+const bookPage = $(".book-page");
+const bookPrice = $(".book-price");
+const bookSummary = $(".book-summary");
 
 getUrlParams();
 showAllElements();
 
 function showAllElements() {
   //헤더 추가
+  alert("ehlsk?");
   productData();
 }
 
 async function productData() {
-  // const { id } = getUrlParams();
-  const { id } = { id: "1" };
+  const { id } = getUrlParams();
+  // const { id } = { id: "1" };
+  console.log(id);
   // const product = await Api.get(`/products/${id}`);
   const product = {
-    title: "미움받을 용기",
-    price: "15,000원",
+    title: "미움받을 용기이이이이이이이이이이",
+    price: "15000",
     category: "인문",
     author: "기시미 이치로 , 고가 후미타케",
     publisher: "인플루엔셜",
     publicationDate: "2014년 11월 17일",
     pageNumber: "197pg",
     summary:
-      "인간은 사회적인 존재다. 그렇기에 아들러는 “인간의 고민은 전부 인간관계에서 비롯된 고민”이라고 말한다. 어떤 종류의 고민이든 거기에는 반드시 타인과의 관계가 얽혀 있게 마련이고, 따라서 행복해지기 위해서는 인간관계로부터 자유로워져야 한다는 것이다. 모든 사람에게 좋은 사람이길 원하는 사람은 타인의 눈치를 볼 수밖에 없다. 이에 아들러는 타인에게 ‘미움받을 용기’를 가져야만 비로소 자유로워지고 행복해진다고 거듭 강조한다.",
+      "인간은 사회적인 존재다. 그렇기에 아들러는 “인간의 고민은 전부 인간관계에서 비롯된 고민”이라고 말한다. 어떤 종류의 고민이든 거기에는 반드시 타인과의 관계가 얽혀 있게 마련이고,",
   };
 
   const {
@@ -76,8 +61,8 @@ async function productData() {
   bookSummary.innerText = summary;
   bookPrice.innerText = `${addCommas(price)}원`;
 
-  window.titleChange("load", async () => {
-    title.innerText = title;
+  window.addEventListener("load", async () => {
+    webTitle.innerText = title;
   });
   addCartBtn.addEventListener("click", async () => {});
   buyNowBtn.addEventListener("click", async () => {});
--- src/views/start/start.js
+++ src/views/start/start.js
@@ -1,2 +1,34 @@
-const startYes = document.querySelector("start-yes");
-const startNo = document.querySelector("start-no");
+import $ from "../utils/dom.js";
+import { navigate } from "../useful-functions.js";
+
+const startBookImg = $(".start-book-img");
+
+const startYesBtn = $(".start-yes");
+const startNoBtn = $(".start-no");
+
+window.addEventListener("load", showStart());
+
+async function showStart() {
+  //책 표지 랜덤으로 불러오기 (id,imgUrl)
+  // const datas = await Api.get("");
+  const { _id, imgUrl } = {
+    _id: "13",
+    imgUrl: "./미움받을용기.jpg", // imgUrl 어케 들고옴?
+  };
+  startBookImg.src = imgUrl;
+
+  function moveDetail() {
+    alert("go product-detail");
+    startYesBtn.href = "/products";
+    // const datas = Api.get(`/api/products/${_id}`);
+    // console.log(datas);
+  }
+
+  function moveHome() {
+    alert("go main");
+    startNoBtn.href = "/home";
+  }
+
+  startYesBtn.addEventListener("click", moveDetail);
+  startNoBtn.addEventListener("click", moveHome);
+}
