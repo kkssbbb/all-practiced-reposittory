c91f9cd9435903c8ef73663c8ca24d3f02812bae
kkssbbb <03337@naver.com>
2022-11-08 14:50:53 +0900

    feat: 주문관리 모달창 수정정

--- src/views/admin-category/admin-category.js
+++ src/views/admin-category/admin-category.js
@@ -2,8 +2,6 @@ const categoryAdd = document.querySelector(".category-add");
 const categoryUpdate = document.querySelectorAll(".category-update");
 const categoryDelete = document.querySelectorAll(".category-delete");
 
-console.log(categoryUpdate);
-
 categoryAdd.addEventListener("click", () => alert("카테고리 추가"));
 categoryUpdate.addEventListener("click", () => alert("카테고리 수정"));
 categoryDelete.addEventListener("click", () => alert("카테고리 삭제"));
