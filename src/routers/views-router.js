3a94816d9a57d6ce0dca176e321a543b133b8f3e
kkssbbb <03337@naver.com>
2022-11-09 23:35:35 +0900

    fix: 장바구니에 상품이 랜덤으로 랜더링되는 이슈 해결

--- src/routers/views-router.js
+++ src/routers/views-router.js
@@ -16,6 +16,7 @@ viewsRouter.use("/login", serveStatic("login"));
 viewsRouter.use("/products/:id", serveStatic("product-detail"));
 
 viewsRouter.use("/cart", serveStatic("cart"));
+viewsRouter.use("/order", serveStatic("order"));
 
 // views 폴더의 최상단 파일인 rabbit.png, api.js 등을 쓸 수 있게 함
 viewsRouter.use("/", serveStatic(""));
