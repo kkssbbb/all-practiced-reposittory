0ee80d9f5c92b460f5473a30d1bbb3b440435c13
kkssbbb <03337@naver.com>
2022-11-09 14:42:22 +0900

    fix: 오더라우터 복구

--- src/routers/order-router.js
+++ src/routers/order-router.js
@@ -39,30 +39,7 @@ orderRouter.post("/orders", loginRequired, async (req, res, next) => {
 //본인 주문 조회
 // 함수로 만들어서 미들웨어에서 사용해도되는지
 //아니면 근야 미들웨어에서 조회로직을 다시짤지
-orderRouter.get("/orders", loginRequired, async (req, res, next) => {});
-
-
-
-//전체 주문조회(관리자)
-orderRouter.get("/orders-list", async function (req, res, next) {
-  console.log("호출확인");
-
-  const orderList = await orderService.getOrders();
-  res.status(200).json(orderList);
-});
-
-//주문 삭제
-orderRouter.delete("/orders/:orderId", async function (req, res, next) {
-  const orderId = req.params.orderId;
-
-  console.log(`파람 값확인: ${orderId}`);
-
-  const deleteOrderInfo = await orderService.deleteOrder(orderId);
-
-  return res.status(201).json(deleteOrderInfo);
-});
-
-orderRouter.patch("/orders/:orderId", async function (req, res, next) {
+orderRouter.get("/orders", loginRequired, async (req, res, next) => {
   try {
     const userId = req.currentUserId;
     console.log(userId);
