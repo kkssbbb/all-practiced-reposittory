ab71ffb5abf045bebd53aeaac409f06f96c28de3
kkssbbb <03337@naver.com>
2022-11-09 21:02:40 +0900

    fix : 주문전일떄만 배송정보 수정가능 찐완

--- src/services/order-service.js
+++ src/services/order-service.js
@@ -44,12 +44,12 @@ class OrderService {
     if (orderinfo !== "배송전") {
       if (orderinfo == "배송중") {
         throw new Error(
-          `상품이 ${orderinfo}이여서 주문정보를 변경할 수 없습니다.^^`
+          `상품이 ${orderinfo}이여서 주문정보를 변경할 수 없습니다.^^늦음ㅅㄱ`
         );
       }
       if (orderinfo == "배송도착") {
         throw new Error(
-          `상품이 ${orderinfo}해서 주문정보를 변경할 수 없습니다.^^`
+          `상품이 ${orderinfo}해서 주문정보를 변경할 수 없습니다.^^늦음ㅅㄱ`
         );
       }
     }
