3f301b97a8a0b0f0db27575b4d34e4dcc179e0d3
kkssbbb <03337@naver.com>
2022-11-11 20:59:13 +0900

    fix: 관리자페이지 카테고리 삭제 기능 추가

--- src/views/admin-category/admin-category.js
+++ src/views/admin-category/admin-category.js
@@ -4,14 +4,21 @@ import $ from "../utils/dom.js";
 // 요소(element), input 혹은 상수
 const titleInput = $("#titleInput");
 const submitButton = $("#addCategoryButton");
-const registerCategoryForm = document.querySelector("#registerCategoryForm");
+const registerCategoryForm = $("#registerCategoryForm");
 
+const nowCategoryInput = $("#nowCategoryInput");
+
+addAllElements();
 addAllEvents();
+
+// html에 요소를 추가하는 함수들
+function addAllElements() {
+  addNowCategory();
+}
 
 // addEventListener들을 묶어주어서 코드를 깔끔하게 하는 역할임.
 function addAllEvents() {
   submitButton.addEventListener("click", handleSubmit);
-  console.log(submitButton);
 }
 
 // 카테고리 추가하기
@@ -37,4 +44,22 @@ async function handleSubmit(e) {
     console.error(err.stack);
     alert(`문제가 발생하였습니다. 확인 후 다시 시도해 주세요: ${err.message}`);
   }
+}
+
+// 카테고리 가져오기
+async function addNowCategory() {
+  const categorys = await Api.get("/api/category");
+  categorys.forEach((item) => {
+    // 객체 destructuring
+    const { _id, category } = item;
+
+    nowCategoryInput.insertAdjacentHTML(
+      "beforeend",
+      `
+        <div id="order-${_id}">
+          <div>${category}</div>
+        </div>
+      `
+    );
+  });
 }
